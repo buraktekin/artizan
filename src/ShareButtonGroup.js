59f38353b65debea0861a3e970e4c27cdcb12f78
buraktekin <tknbrk@gmail.com>
2020-04-05 09:51:13 +0200

    🗂 add social media sharing module

---
+++ src/ShareButtonGroup.js
@@ -0,0 +1,56 @@
+// ShareButtonGroup
+import React from "react"
+import { Grid, Button, Popup } from "semantic-ui-react"
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share"
+
+const ShareButtonGroup = (props) => {
+  const { shares, shareUrl, handlers } = props
+
+  return (
+    <Popup wide trigger={
+      <Button
+        basic
+        color='blue'
+        content='Share'
+        icon='paper plane'
+        label={{
+          as: 'a',
+          basic: true,
+          color: 'blue',
+          pointing: 'left',
+          content: shares,
+        }}
+      />
+    } on='click'>
+      <Grid divided columns='equal'>
+        <Grid.Column>
+          <WhatsappShareButton url={shareUrl}>
+            <WhatsappIcon size={32} round={true} />
+            {shareCount => <span className="myShareCountWrapper">{shareCount}</span>}
+          </WhatsappShareButton>
+        </Grid.Column>
+        <Grid.Column>
+          <FacebookShareButton url={shareUrl}>
+            <FacebookIcon size={32} round={true} />
+            {shareCount => <span className="myShareCountWrapper">{shareCount}</span>}
+          </FacebookShareButton>
+        </Grid.Column>
+        <Grid.Column>
+          <TwitterShareButton url={shareUrl} onClick={handlers.handleShare}>
+            <TwitterIcon size={32} round={true} />
+            {shareCount => <span className="myShareCountWrapper">{shareCount}</span>}
+          </TwitterShareButton>
+        </Grid.Column>
+      </Grid>
+    </Popup>
+  )
+}
+
+export default ShareButtonGroup
